0258694ef0497814fc199ae1f20b9968145315f2
love98 <liaoliaocaicai@163.com>
2023-11-14 16:58:52 +0800

    Rename sast-evento-mobile -> Sast-Evento-Mobile

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,10 +2,10 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'sast.evento.mobile',
-  appName: 'sast-evento-mobile',
+  appName: 'Sast-Evento-Mobile',
   webDir: 'dist',
   server: {
-    androidScheme: 'https'
+    androidScheme: 'http'
   }
 };
 
